0892b7a9b053d4d779224c399ddc6c3d423072dd
juan david gaviria guzman <jd58_2g@hotmail.com>
2024-11-01 18:21:35 -0500

    litte modication

--- ejemplo.py
+++ ejemplo.py
@@ -2,6 +2,6 @@ def saludar (nombre):
 
     return f'Hola {nombre}'
 
-nombre_usuario = input ("cual es tu nombre")
+nombre_usuario = input ("cual es tu nombre?  ")
 saludo = saludar(nombre_usuario)
 print(saludo)
